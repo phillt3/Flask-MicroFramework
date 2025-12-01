e5f5a57be02f22a3c0fd4f8d9b80657ccc980fd7
Phillip Tracy <tracyphill3@gmail.com>
2025-12-01 12:07:47 -0500

    Modifying DB models, insertions, deletions, querying

--- tutorial 7.py
+++ tutorial 7.py
@@ -24,12 +24,25 @@ class users(db.Model):
 def home():
     return render_template("index.html")
 
+@app.route("/view")
+def view():
+    return render_template("view.html", values=users.query.all())
+
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
+        
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+            
         flash("Login successful.")
         return redirect(url_for("user"))
     else:
@@ -46,6 +59,9 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
             flash("Email was saved!")
         else:
             if "email" in session:
@@ -63,5 +79,6 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
